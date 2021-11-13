2cb22a33e496551d5d74720f6026e85b6f2df5a6
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-13 08:03:39 +0000

    not a bad statebins plot of imports and exports

--- plots/dog-moves_plot.R
+++ plots/dog-moves_plot.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(here)
+library(patchwork)
 library(readr)
 library(statebins)
 library(tidyr)
@@ -9,15 +10,42 @@ library(viridis)
 dog_moves <- read_csv(here("data-raw", "dog_moves.csv"), show_col_types = FALSE)
 
 dog_moves_ <- dog_moves %>%
-  replace_na(list(exported = 0, imported = 0))
+  filter(inUS) %>%
+  filter(location!="Washington DC") %>%
+  replace_na(list(exported = 0, imported = 0)) %>%
+  mutate(
+    x = (exported - imported)/total
+  )
 
-dog_moves %>%
-  ggplot(aes(state = location, fill = (exported - imported)/total)) +
+dogs_imported <- dog_moves_ %>%
+  rename(value = imported) %>%
+  ggplot(aes(state = location, fill = value))+
   geom_statebins() +
   scale_fill_viridis(
     option = "magma", direction = -1
   ) +
   theme_statebins() +
-  coord_equal()
+  coord_equal() +
+  labs(
+    title = "imported"
+  )
+
+dogs_exported <- dog_moves_ %>%
+  rename(value = exported) %>%
+  ggplot(aes(state = location, fill = value))+
+  geom_statebins() +
+  scale_fill_viridis(
+    option = "magma", direction = -1
+  ) +
+  theme_statebins() +
+  coord_equal() +
+  labs(
+    title = "exported"
+  )
+
+dogs_imported + dogs_exported +
+  plot_layout(guides = "collect") &
+  scale_fill_viridis(option = "magma", direction = -1, limits = range(c(0, 1000))) &
+  theme(legend.position = "bottom")
 
 ggsave(filename = here("plots", "dog-moves.png"))
