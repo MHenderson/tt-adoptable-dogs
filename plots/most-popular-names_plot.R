662d3634af50419364a44cc01f2cab7cbea36f4d
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-13 10:10:26 +0000

    colour name by number of syllables

--- plots/most-popular-names_plot.R
+++ plots/most-popular-names_plot.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(here)
 library(readr)
 
+f1 <- "Roboto Condensed"
+
 dog_descriptions <- read_csv(here("data-raw", "dog_descriptions.csv"), show_col_types = FALSE)
 
 top_fifty_names <- dog_descriptions %>%
@@ -12,20 +14,28 @@ top_fifty_names <- dog_descriptions %>%
   group_by(name) %>%
   count() %>%
   arrange(desc(n)) %>%
-  head(50)
+  head(100) %>%
+  ungroup()
+
+top_fifty_names <- top_fifty_names %>%
+  mutate(
+    syllables = as.numeric(count_vector(top_fifty_names$name))
+  )
 
 top_fifty_names %>%
   ggplot(aes(reorder(name, n), n)) +
-  geom_text(aes(label = name)) +
+  geom_text(aes(label = name, colour = as.factor(syllables)), family = f1) +
   coord_flip() +
   labs(
-    x = "",
-    y = ""
+    y = "Number of dogs with name",
+    x = ""
   ) +
   ylim(50, 300) +
+  theme_minimal() +
   theme(
     axis.text.y = element_blank(),
     panel.grid.major.y = element_blank()
-  )
+  ) +
+  annotate(geom = "text", x = 50, y = 130, label = "One Hundred\nMost Popular\nDog Names", size = 15, hjust = 0, family = f1)
 
-ggsave(filename = here("plots", "most-popular-names.png"))
+ggsave(filename = here("plots", "most-popular-names.png"), height = 4000, width = 2000, units = "px", bg = "white")
