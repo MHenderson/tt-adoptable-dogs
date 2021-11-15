a809eb68e33b4f9f2acc68ea7f86a5d79d67d0e4
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-15 13:02:59 +0000

    a heatmap for most popular breeds

--- plots/most-popular-breeds.R
+++ plots/most-popular-breeds.R
@@ -8,34 +8,22 @@ dog_descriptions <- read_csv(here("data-raw", "dog_descriptions.csv"), show_col_
 
 X <- dog_descriptions %>%
   filter(contact_state %in% state.abb) %>%
-  mutate(
-    breed = case_when(
-      breed_mixed & !is.na(breed_secondary) ~ paste0(breed_primary, " x ", breed_secondary),
-      TRUE ~ breed_primary
-    )
-  ) %>%
-  group_by(contact_state, breed) %>%
-  count(breed) %>%
-  ungroup() %>%
-  group_by(contact_state) %>%
-  slice_max(n, n = 5) %>%
-  ungroup() %>%
-  arrange(contact_state, n) %>%
-  mutate(order = row_number())
+  group_by(contact_state, breed_primary) %>%
+  count(breed_primary)
 
-agg_png(here("plots", "most-popular-breeds.png"), height = 2000, width = 4000, units = "px")
+agg_png(here("plots", "most-popular-breeds.png"), res = 320, height = 10, width = 26, units = "in")
 
 p <- X %>%
-  ggplot(aes(x = order, y = n)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~contact_state, scales = "free", ncol = 8) +
-  scale_x_continuous(
-    breaks = X$order,
-    labels = X$breed,
-    expand = c(0,0)
+  ggplot(aes(breed_primary, contact_state)) +
+  geom_raster(aes(fill = n), hjust = 1) +
+  scale_y_discrete(limits = rev) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    axis.text.x = element_text(angle = 90, hjust = 1)
   ) +
-  coord_flip() +
-  theme_minimal()
+  scale_fill_viridis() +
+  labs(x = "", y = "")
 
 print(p)
 
