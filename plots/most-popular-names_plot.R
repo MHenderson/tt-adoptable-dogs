b66e844b81f3cd535746895df29efc2ffb4ed4b2
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-16 13:30:57 +0000

    finish the most popular dogs name plot

--- plots/most-popular-names_plot.R
+++ plots/most-popular-names_plot.R
@@ -3,9 +3,11 @@ library(ggplot2)
 library(here)
 library(ragg)
 library(readr)
+library(RColorBrewer)
 library(syllable)
 
 f1 <- "Roboto Condensed"
+f2 <- "Cardo"
 
 dog_descriptions <- read_csv(here("data-raw", "dog_descriptions.csv"), show_col_types = FALSE)
 
@@ -26,20 +28,27 @@ agg_png(here("plots", "most-popular-names.png"), res = 320, height = 14, width =
 
 p <- top_fifty_names %>%
   ggplot(aes(reorder(name, n), n)) +
-  geom_text(aes(label = name, colour = as.factor(syllables)), family = f1) +
+  geom_text(aes(label = name, colour = as.factor(syllables)), family = f2, size = 5, fontface = "bold", alpha = .9) +
   coord_flip() +
-  labs(
-    y = "Number of dogs with name",
-    x = ""
-  ) +
   ylim(50, 300) +
+  scale_color_brewer(type = "qual") +
+  labs(
+    y = "Number of dogs",
+    x = "",
+    title = NULL,
+    subtitle = NULL,
+    caption = "Graphic: Matthew Henderson"
+  ) +
   theme_minimal() +
   theme(
+    axis.title.x = element_text(hjust = 1, margin = margin(10, 0, 0, 0)),
     axis.text.y = element_blank(),
     panel.grid.major.y = element_blank(),
-    legend.position = "none"
+    legend.position = "none",
+    plot.caption = element_text(hjust = 0)
   ) +
-  annotate(geom = "text", x = 50, y = 130, label = "One Hundred\nMost Popular\nDog Names", size = 15, hjust = 0, family = f1)
+  annotate(geom = "text", x = 60, y = 125, label = "One Hundred\nMost Popular\nDog Names", size = 18, hjust = 0, family = f1, fontface = "bold") +
+  annotate(geom = "text", x = 40, y = 125, label = "Data collected from the PetFinder API\nby Amber Thomas for all adoptable dogs\nin each state on September 20, 2019.", size = 6, hjust = 0, family = f1)
 
 print(p)
 
