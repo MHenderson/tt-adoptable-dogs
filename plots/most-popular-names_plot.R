4c45e4de9f1ee62e19c58cff52b452c22c44ade9
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-17 09:19:26 +0000

    finishing touches

--- plots/most-popular-names_plot.R
+++ plots/most-popular-names_plot.R
@@ -37,18 +37,17 @@ p <- top_fifty_names %>%
     x = "",
     title = NULL,
     subtitle = NULL,
-    caption = "Graphic: Matthew Henderson"
+    caption = "Data: Collected from the PetFinder API on September 20, 2019 by Amber Thomas\nGraphic: Matthew Henderson\nCode: https://github.com/MHenderson/adoptable-dogs"
   ) +
   theme_minimal() +
   theme(
-    axis.title.x = element_text(hjust = 1, margin = margin(10, 0, 0, 0)),
+    axis.title.x = element_text(hjust = 1, margin = margin(10, 0, 0, 0), family = f1),
     axis.text.y = element_blank(),
     panel.grid.major.y = element_blank(),
     legend.position = "none",
-    plot.caption = element_text(hjust = 0)
+    plot.caption = element_text(size = 10, family = f1, hjust = 0, margin = margin(20, 0, 0, 0))
   ) +
-  annotate(geom = "text", x = 60, y = 125, label = "One Hundred\nMost Popular\nDog Names", size = 18, hjust = 0, family = f1, fontface = "bold") +
-  annotate(geom = "text", x = 40, y = 125, label = "Data collected from the PetFinder API\nby Amber Thomas for all adoptable dogs\nin each state on September 20, 2019.", size = 6, hjust = 0, family = f1)
+  annotate(geom = "text", x = 60, y = 125, label = "One Hundred\nMost Popular\nDog Names", size = 18, hjust = 0, family = f1, fontface = "bold")
 
 print(p)
 
