1a096fad689aeec2994083ff284d9f2e3c7f821d
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-11-21 16:18:58 +0000

    use renv and targets

--- data-raw/download_data.R
+++
@@ -1,5 +0,0 @@
-library(here)
-
-dog_moves <- download.file('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_moves.csv', destfile = here("data-raw", "dog_moves.csv"))
-dog_travel <- download.file('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv', destfile = here("data-raw", "dog_travel.csv"))
-dog_descriptions <- download.file('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_descriptions.csv', destfile = here("data-raw", "dog_descriptions.csv"))
